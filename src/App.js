5b039e7d06d5ef38f713af62085dcf59f783d4e4
kacemabidi12 <163314599+kacemabidi12@users.noreply.github.com>
2024-05-20 14:47:34 +0100

    added few things and did the Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import UserInfo from './UserInfo';
 import Jeux from './Jeux-Page';
 
 const App = () => {
   return (
-    <div className="flex font-fredoka bg-bg-purple-ref">
-      <Navbar />
-      <Jeux />
-    </div>
+    <Router>
+      <div className="flex font-fredoka bg-bg-purple-ref">
+        <Navbar />
+        <Routes>
+          <Route path="/profile" element={<UserInfo />} />
+          <Route path="/jeux" element={<Jeux />} />
+          <Route path="/" element={<Jeux />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
